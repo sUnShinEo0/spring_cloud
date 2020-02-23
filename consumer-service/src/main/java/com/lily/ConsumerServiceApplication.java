f3fb62ccd5f1635f3f77e85169737eb60cfa5afd
liling963@emao.com <liling963@emao.com>
2020-02-23 18:05:29 +0800

    消费者service创建注册, 并且访问提供者服务

--- consumer-service/src/main/java/com/lily/ConsumerServiceApplication.java
+++ consumer-service/src/main/java/com/lily/ConsumerServiceApplication.java
@@ -1,16 +1,25 @@
-package com.lily.providerservice;
+package com.lily;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ProviderServiceApplication {
+public class ConsumerServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProviderServiceApplication.class, args);
+        SpringApplication.run(ConsumerServiceApplication.class, args);
     }
+
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
 
 }
---
+++ consumer-service/src/main/java/com/lily/controller/ConsumerController.java
@@ -0,0 +1,38 @@
+package com.lily.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+/**
+ * @author: liling
+ * @date: Created in 2020/2/23
+ * @description:
+ * @version:1.0
+ */
+@RestController
+@RequestMapping("/user")
+public class ConsumerController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    //注意这里的导包不要搞错
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/findAll")
+    private String findAll() {
+        List<ServiceInstance> instances = discoveryClient.getInstances("provider-service");
+        ServiceInstance instanceInfo = instances.get(0);
+        String hostName = instanceInfo.getHost();
+        int port = instanceInfo.getPort();
+        return restTemplate.getForObject("http://" + hostName +":"+ port + "/user/findAll", String.class);
+    }
+
+}
